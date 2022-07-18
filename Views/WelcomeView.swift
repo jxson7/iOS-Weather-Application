d88518a9235c9ae7a7a0f1ea75267cc579a56195
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-07-18 22:36:34 +0100

    Initial Commit

---
+++ Views/WelcomeView.swift
@@ -0,0 +1,41 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Jason Dookarun on 18/07/2022.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationmanager : LocationManager
+    
+    var body: some View {
+        VStack{
+            VStack(spacing : 20){
+                Text("Welcome to the Weather App").bold().font(.title)
+                
+                Text("Please share your current location to get the weather in your area").padding()
+            
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationmanager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+}
+}
+
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jason Dookarun on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
